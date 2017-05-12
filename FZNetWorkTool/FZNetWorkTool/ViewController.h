b8bc4f8ee81a8921e53b96369050d767b1d639e2
AllureTeartop <934190039@qq.com>
2017-05-12 11:43:43 +0800

    1.项目初始化

---
+++ FZNetWorkTool/FZNetWorkTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FZNetWorkTool
+//
+//  Created by Florence on 2017/5/12.
+//  Copyright © 2017年 AllureTeartop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FZNetWorkTool/FZNetWorkTool/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FZNetWorkTool
+//
+//  Created by Florence on 2017/5/12.
+//  Copyright © 2017年 AllureTeartop. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
